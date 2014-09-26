4ec7f79ab692a43690c14d4702284e88342fd82a
Aman Singhal <aman.singhal@dtu.co.in>
2014-09-26 10:53:41 +0530

    Created a blank project

---
+++ FlickrApp/FlickrApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace FlickrApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Navigates to about page.
+        /// </summary>
+        private void GoToAbout(object sender, GestureEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/About.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
